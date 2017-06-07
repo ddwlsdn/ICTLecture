1dbb3315b8afb7dd3d162774ec22c795334888ff
unknown <>
2017-06-08 04:09:49 +0900

    middle test

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,14 @@
-module.exports = function(app)
-{
-     app.get('/',function(req,res){
-        res.render('index.html')
-     });
-    app.get('/profile',function(req,res){
-	   res.render('profile.html')
-    });
-    app.get('/board',function(req,res){
-	   res.render('board.html')
-    });
-}
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req,res){
+    res.render('index')
+});
+router.get('/index',function(req,res){
+    res.render('index')
+ });
+ router.get('/profile',function(req,res){
+     res.render('profile')
+});
+
+module.exports = router;
